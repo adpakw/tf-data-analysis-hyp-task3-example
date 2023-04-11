d31cf71b173ffbf1be75725277fbc32461cb7523
adpakw <94380412+adpakw@users.noreply.github.com>
2023-04-11 14:14:24 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,20 @@
 import pandas as pd
 import numpy as np
-
+from scipy.stats import t
 
 chat_id = 123456 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(control: np.array, test: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+    if control.size == 0 or test.size == 0:
+        return False
+    x1 = np.mean(control)
+    x2 = np.mean(test)
+    s1 = np.std(control, ddof=1)
+    s2 = np.std(test, ddof=1)
+    n1 = control.size
+    n2 = test.size
+    t_stat = (x1 - x2) / np.sqrt(s1**2/n1 + s2**2/n2)
+    alpha = 0.07
+    df = n1 + n2 - 2
+    t_alpha = t.ppf(1 - alpha/2, df)
+    return t_stat > t_alpha
